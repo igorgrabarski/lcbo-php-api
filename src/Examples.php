d0a2476d834f95b3c75120edb1caa3df993a5c7e
Igor Grabarski <igorgrabarski@gmail.com>
2017-12-20 18:48:10 -0500

    added comments to Examples.php

--- src/Examples.php
+++ src/Examples.php
@@ -1,42 +1,46 @@
 <?php
 
 
+// Add composer's autoloader
 require dirname( __DIR__ ) . '/vendor/autoload.php';
 
 
 use igorgrabarski\classes\Store;
 use igorgrabarski\LCBO;
 
+// Add your API_KEY in .env file!!!
+
+// Create instance of LCBO class
 $lcbo = new LCBO();
 
- $stores = $lcbo->getStores();
+// Get all stores
+$stores = $lcbo->getStores();
+print_r($stores);
 
- print_r($stores);
+// Get particular store by id
+$store = $lcbo->getStore(31);
+print_r($store);
 
-//$store = $lcbo->getStore(31);
-//
-//print_r($store);
+// Get all products
+$products = $lcbo->getProducts();
+print_r($products);
 
-//$products = $lcbo->getProducts();
-//
-//print_r($products);
+// Get particular product by id
+$product = $lcbo->getProduct(311787);
+print_r($product);
 
-//$product = $lcbo->getProduct(311787);
-//
-//print_r($product);
+// Get inventory by store_id and product_id
+$inventory = $lcbo->getInventory(329, 518415);
+print_r($inventory);
 
-//$inventory = $lcbo->getInventory(329, 518415);
-//
-//print_r($inventory);
+// Get all inventories
+$inventories = $lcbo->getInventories();
+print_r($inventories);
 
-//$inventories = $lcbo->getInventories();
-//
-//print_r($inventories);
+// Get dataset by id
+$dataset = $lcbo->getDataset(2419);
+print_r($dataset);
 
-//$dataset = $lcbo->getDataset(2419);
-//
-//print_r($dataset);
-
-//$datasets = $lcbo->getDatasets();
-//
-//print_r($datasets);
+// Get all datasets
+$datasets = $lcbo->getDatasets();
+print_r($datasets);
